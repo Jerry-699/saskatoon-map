d9acb1805371e3e706f1d25f1970d0c2e3025ce6
Jerry-699 <Faransiddiqui300@gmail.com>
2026-08-29 02:48:31 -0600

    Add files via upload

---
+++ saskatoon-secure-map/app/check-email/page.tsx
@@ -0,0 +1,2 @@
+import Link from 'next/link'
+export default function Page(){return <main className="shell"><div className="card"><h1>Check your email</h1><p>Open the confirmation email, then sign in. Your account will still need administrator approval.</p><Link className="button" href="/login">Go to sign in</Link></div></main>}
